3d27bade01273e845f6ecf5c608127a560f76f31
beaever <thebeaever@gmail.com>
2021-07-23 02:14:35 +0900

    0723_TodoList Study

---
+++ src/components/TodoCreate.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {MdAdd} from 'react-icons/md'
+
+const CircleButton = styled.button`
+  background: #38d9a9;
+  &:hover {
+    background : #63e6be;
+  }
+  &:hover {
+    background: #20c997;
+  }
+
+  z-index: 5;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justifiy-content: center;
+
+  position: absolute;
+  left: 50%;
+  bottom: 0px;
+  transform: translate(-50%, 50%);
+
+  font-size: 60px;
+  color: white;
+  border-radius: 40px;
+
+  border: none;
+  outline: none;
+`;
+
+function TodoCreate() {
+
+  return (
+    <>
+    <CircleButton>
+      <MdAdd / >
+    </CircleButton>
+      </>
+  );
+}
+
+export default TodoCreate;
